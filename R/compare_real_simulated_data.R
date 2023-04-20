9a905e779470c2b2c7b185e27278b22775ad7ce1
AlexWeinreb <alexis.weinreb@yale.edu>
2023-04-20 17:33:25 -0400

    Also compare simulation v1

--- R/compare_real_simulated_data.R
+++ R/compare_real_simulated_data.R
@@ -21,7 +21,7 @@ logit <- function(x){
 
 
 
-## Simulated data ----
+## Simulated data v7 ----
 
 
 quantifs_filtered_sim <- qs::qread("data/intermediates/230413_simulation_v7/quantifs_filtered.qs")
@@ -52,6 +52,40 @@ mat_sf_sim <- mat_sf_sim[,! apply(mat_sf_sim, 2, \(col) any(is.na(col)))]
 quantifs_filtered_sim
 dim(mat_sf_sim)
 mat_sf_sim[1:3,1:3]
+
+
+
+## Simulated data v1 ----
+
+
+quantifs_filtered_sim1 <- qs::qread("data/intermediates/simultation/sim_quantifs.qs")
+sf_sim1 <- qs::qread("data/intermediates/simultation/sim_sf.qs")
+true_coefs_sim1 <- qs::qread("data/intermediates/simultation/true_coefs.qs")
+
+
+
+quantifs_filtered_sim1 <- quantifs_filtered_sim1 |>
+  group_by(event_id) |>
+  mutate(dPSI_nat = PSI - mean(PSI),
+         dPSI_logit = logit(PSI) - logit(mean(PSI)))
+
+
+# Make SF expression as a matrix for use in regression
+mat_sf_sim1 <- sf_sim1 |>
+  select(transcript_id, sample_id, TPM) |>
+  mutate(TPM = log(TPM + 1)) |>
+  pivot_wider(id_cols = sample_id,
+              names_from = "transcript_id",
+              values_from = "TPM") |>
+  column_to_rownames("sample_id") |>
+  as.matrix() |>
+  scale()
+mat_sf_sim1 <- mat_sf_sim1[,! apply(mat_sf_sim1, 2, \(col) any(is.na(col)))]
+
+
+quantifs_filtered_sim1
+dim(mat_sf_sim1)
+mat_sf_sim1[1:3,1:3]
 
 
 
@@ -220,7 +254,7 @@ qs::qsave(permutations_real, "data/intermediates/230413_simulation_v7/permutatio
 
 
 
-# simulated data
+# simulated data v7
 permutations_sim <- expand_grid(event_id = unique(quantifs_filtered_sim$event_id),
                                  method = c("lasso"),
                                  column = c("dPSI_nat"),
@@ -239,6 +273,29 @@ permutations_sim <- expand_grid(event_id = unique(quantifs_filtered_sim$event_id
   select(-res)
 
 qs::qsave(permutations_sim, "data/intermediates/230413_simulation_v7/permutations_sim_singleUnshuffled_100.qs")
+
+
+# simulated data v1
+permutations_sim1 <- expand_grid(event_id = unique(quantifs_filtered_sim1$event_id),
+                                method = c("lasso"),
+                                column = c("dPSI_nat"),
+                                shuffle = c(rep(FALSE, 1), rep(TRUE, 100))) |>
+  mutate(res = pmap(list(event_id, method, column, shuffle),
+                    \(event_id, method, column, shuffle) do_regression(event_id,
+                                                                       method, column,
+                                                                       shuffle, mat_sf_sim1, quantifs_filtered_sim1,
+                                                                       intercept = FALSE),
+                    .progress = TRUE),
+         fit = map(res, ~pluck(.x, "fit", .default = NA)),
+         rsquare = map_dbl(res, ~pluck(.x, "rsquare", .default = NA_real_)),
+         nb_coefs = map_int(res, ~pluck(.x, "nb_coefs", .default = NA_integer_)),
+         prediction_on_test = map(res, ~pluck(.x, "prediction_on_test", .default = tibble())),
+         coefs_sf = map(res, ~pluck(.x, "coefs_sf", .default = tibble()))) |>
+  select(-res)
+
+qs::qsave(permutations_sim1, "data/intermediates/230413_simulation_v7/permutations_sim1_singleUnshuffled_100.qs")
+
+
 
 
 #~ reformulate ----
@@ -321,11 +378,52 @@ res_sim <- left_join(perm_effect_size_sim,
                       perm_p_val_sim,
                       by = c("event_id", "transcript_id"))
 
+
+
+perm_effect_size_sim1 <- permutations_sim1 |>
+  select(event_id, shuffle, coefs_sf) |>
+  unnest(coefs_sf) |>
+  group_by(shuffle, event_id, transcript_id) |>
+  summarize(mean_s = mean(s1),
+            .groups = "drop") |>
+  pivot_wider(id_cols = c(event_id, transcript_id),
+              names_from = "shuffle",
+              values_from = "mean_s") |>
+  rename(coef = `FALSE`,
+         mean_null = `TRUE`) |>
+  mutate(coef_effect_size = coef - mean_null)
+
+
+
+
+perm_p_val_sim1 <- permutations_sim1 |>
+  select(event_id, shuffle, coefs_sf) |>
+  unnest(coefs_sf) |>
+  pivot_wider(id_cols = c(event_id, transcript_id),
+              names_from = "shuffle",
+              values_from = "s1",
+              values_fn = list) |>
+  rename(coef = `FALSE`,
+         coefs_null = `TRUE`) |>
+  rowwise() |>
+  mutate(p_val = sum(abs(coefs_null) >= abs(coef))/length(coefs_null)) |>
+  ungroup() |>
+  mutate(p_adj = p.adjust(p_val, "BH")) |>
+  select(event_id, transcript_id, p_val, p_adj)
+
+
+
+res_sim1 <- left_join(perm_effect_size_sim1,
+                     perm_p_val_sim1,
+                     by = c("event_id", "transcript_id"))
+
+
+
 # Look at results
 
 permutations_real
 permutations_sim
-
+permutations_sim1
 
 table(is.na(permutations_real$rsquare))
 hist(permutations_real$nb_coefs)
@@ -334,21 +432,25 @@ table(is.na(permutations_sim$rsquare))
 hist(permutations_sim$nb_coefs)
 
 
+table(is.na(permutations_sim1$rsquare))
+hist(permutations_sim1$nb_coefs)
+
 res_real
 res_sim
-
+res_sim1
 
 
 table(res_real$p_adj)
 
 table(res_sim$p_adj)
+table(res_sim1$p_adj)
 
 
 res_sim |>
   filter(p_adj == 0)
 
-xx <- res_sim |>
-  left_join(true_coefs_sim,
+xx <- res_sim1 |>
+  left_join(true_coefs_sim1,
             by = c("event_id", "transcript_id"))
 
 
@@ -364,7 +466,7 @@ xx |>
   filter(!is.na(true_coef)) |>
   ggplot() +
   theme_classic() +
-  geom_point(aes(true_coef, y = coef_effect_size, shape = contribution, color = p_adj == 0))
+  geom_point(aes(true_coef, y = coef_effect_size, color = p_adj == 0))
   
 
 
