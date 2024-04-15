6bd634cf62eaf768c2acd2fd4c5d7e0b347d2967
AlexWeinreb <alexis.weinreb@yale.edu>
2024-04-15 18:53:38 -0400

    create job final

---
+++ src/dsq_create_final.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=day
+#SBATCH --job-name=dsq_create
+#SBATCH -c 1
+#SBATCH --mem=100M
+#SBATCH --time=00:02:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alexis.weinreb@yale.edu
+
+set -ue
+
+ml dSQ
+
+dsq --job-file joblists/240415_final.txt --mem 5G --time 01:30:00 --partition day
+
